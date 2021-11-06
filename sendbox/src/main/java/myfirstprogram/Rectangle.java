f7963aad3a2a202eaa74861ce9b0e7a050b6aa75
VadVVA199 <vvv.wolf@gmail.com>
2021-11-06 13:07:54 +0300

    Создание методов в классах.

--- sendbox/src/main/java/myfirstprogram/Rectangle.java
+++ sendbox/src/main/java/myfirstprogram/Rectangle.java
@@ -3,11 +3,14 @@ package myfirstprogram;
 public class Rectangle {
 	public double a;
 	public double b;
-	
+
 	public Rectangle(double a, double b) {
 		this.a = a;
 		this.b = b;
-		
+
 	}
-	
+
+	public double area() {
+		return this.a * this.b;
+	}
 }
--- sendbox/src/main/java/myfirstprogram/Square.java
+++ sendbox/src/main/java/myfirstprogram/Square.java
@@ -2,9 +2,13 @@ package myfirstprogram;
 
 public class Square {
 	public double l;
-	
-	public Square(double l){
-	this.l = l;
+
+	public Square(double l) {
+		this.l = l;
+	}
+
+	public double area() {
+		return this.l * this.l;
 	}
 
 }
